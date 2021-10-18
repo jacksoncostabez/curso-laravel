af42c62dc734d23934046607c2f364b4dad18f05
Jackson <jackson.costabezerra@gmail.com>
2021-10-18 18:35:07 -0300

    Commit Upload Files And Migrations DB

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUpdateProductRequest;
 use Illuminate\Http\Request;
 
 class TesteController extends Controller
@@ -36,12 +37,36 @@ class TesteController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  App\Http\Requests\StoreUpdateProductRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreUpdateProductRequest $request)
     {
-        dd('Cadastrando...');
+        /*Essa é uma das formas mais simples de validar. E não é a forma recomendada porque trás muita informação
+        para o controller. E ele deve ser mais simples possível! */
+       
+        /* $request->validate([
+            'name' => 'required|min:3|max:255',
+            'description' => 'nullable|min:3|max:10000',
+            'photo' => 'required|image',
+        ]); */
+
+        //dd($request->all()); // pega todos os dados cadastrados no form. (mais usado)
+        //dd($request->only(['name', 'description'])); // pega o nome e a descrição
+        //dd($request->name); //pega somente o name (mais usado)
+        //dd($request->has('name')); // retorna true se existir esse campo
+        //dd($request->input('name', 'default')); //adiciona um valor default ao name caso o name não exista.
+        //dd($request->except('_token')); // pega todos os dados, exceto o token.
+        //dd($request->file('photo')); // retorna todos os dados do arquivo enviado
+        //dd($request->file('photo')->isValid()); // verifica se o arquivo é válido para upload.
+        //dd($request->photo->extension()); // mostra extensão do arquivo
+        //dd($request->photo->getClientOriginalName()); // retorna o nome original do arquivo.
+        if($request->file('photo')->isValid()) {
+            //dd($request->file('photo')->store('products')); //products é a pasta onde vai ficar o arquivo storage/app/products. Ele salva com o nome padrão do arquivo
+            $nameFile = $request->name . '.' . $request->photo->extension();
+            //dd($request->file('photo')->storeAs('public/products', $nameFile)); //Salva o arquivo com nome personalizado dentro do diretório privado em storage/products
+            dd($request->file('photo')->storeAs('public/products', $nameFile)); //Salva o arquivo com nome personalizado dentro do diretório storage/public/products
+        }
     }
 
     /**
--- resources/views/admin/pages/testes/create.blade.php
+++ resources/views/admin/pages/testes/create.blade.php
@@ -3,10 +3,23 @@
 @section('content')
     <h1>Cadastrar Novo Produto</h1>
 
-    <form action="{{ route('teste.store') }}" method="post">
+    @if ($errors->any())
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    @endif
+    {{-- enctype="multipart/form-data" >> Permite fazer upload de arquivos --}}
+    <form action="{{ route('teste.store') }}" method="post" enctype="multipart/form-data">
         @csrf {{-- Cria um campo com token para validar a requisição e evitar ataque --}}
-        <input type="text" name="name" placeholder="Nome: ">
-        <input type="text" name="description" placeholder="Descrição: ">
+        <input type="text" name="name" placeholder="Nome: " value="{{ old('name') }}">
+        <input type="text" name="description" placeholder="Descrição: " value="{{ old('description') }}">
+        <input type="file" name="photo">
+        <br><br>
         <button type="submit">Cadastrar</button>
     </form>
+
+    {{-- value="{{ old('name') }} => Esse método deixa os campos preenchidos após um erro ao preencher o form --}}
+
 @endsection
